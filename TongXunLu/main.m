02ee04070f99bbd68ce7e1839a21c2b7d7d1dfc4
teach <apple@S808-27.local>
2014-08-31 12:54:49 +0800

    Initial Commit

---
+++ TongXunLu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TongXunLu
+//
+//  Created by teach on 14-8-31.
+//  Copyright (c) 2014年 dhb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "txlAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([txlAppDelegate class]));
+    }
+}
---
+++ TongXunLu/txlAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  txlAppDelegate.h
+//  TongXunLu
+//
+//  Created by teach on 14-8-31.
+//  Copyright (c) 2014年 dhb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface txlAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
